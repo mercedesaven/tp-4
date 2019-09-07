3f380defa9df27c3717f85ed91734f7217eb967b
mercedesaven <mercedes.aven@gmail.com>
2019-09-06 21:06:13 -0300

    2 commit

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,22 @@
-const user = [{name:"calixta", emial:"calix@calix.com"},
-{name:"calixta", emial:"calix@calix.com"},
-{name:"calixta", emial:"calix@calix.com"}
+const user = [{
+    nombre: 'Ada',
+    apellido: 'Lovelace',
+    telefono: '1234567890',
+    email: 'contacto@gmail.com'
+  },
+   {
+    nombre: 'Grace',
+    apellido: 'Hopper',
+    telefono: '087654321',
+    email: 'contacto@hotmail.com'
+  },
+  {
+    nombre: 'pepita',
+    apellido: 'la pistolera',
+    telefono: '233389211',
+    email: 'contacto@hotmail.com'
+  }
+
 
 ]
 
