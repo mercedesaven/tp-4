3caf29e82dffa5a624048c378e782caa3ba7cc60
Yesica Lopez <yesilop@ar.ibm.com>
2019-09-08 21:54:00 -0300

    no me funciona node

---
+++ public/scripts.js
@@ -0,0 +1,18 @@
+const onLoad = () => {
+    let openModal = document.getElementById('modal')
+  } 
+
+
+//Modal
+
+const modal = () => {
+    let activeModal = document.getElementById('activeModal')
+    activeModal.classList.remove('modal')
+    activeModal.classList.add('activeModal')
+    }
+    
+    const closeModal = () => {
+      let closeModal = document.getElementById('activeModal')
+      closeModal.classList.remove('activeModal')
+      closeModal.classList.add('modal')
+    }
