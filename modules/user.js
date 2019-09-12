98ca473e84a981f7f8ef5b144f30906576d2e053
mercedesaven <mercedes.aven@gmail.com>
2019-09-12 00:03:24 -0300

    con li

--- modules/user.js
+++ modules/user.js
@@ -1,21 +1,21 @@
 const user = [{
-    nombre: 'Ada',
-    apellido: 'Lovelace',
-    telefono: '1234567890',
-    email: 'contacto@gmail.com'
-  },
-   {
-    nombre: 'Grace',
-    apellido: 'Hopper',
-    telefono: '087654321',
-    email: 'contacto@hotmail.com'
-  },
-  {
-    nombre: 'pepita',
-    apellido: 'la pistolera',
-    telefono: '233389211',
-    email: 'contacto@hotmail.com'
-  }
+  name: 'Ada',
+  email: 'ada@gmail.com',
+  adress: 'avenida siempreviva 742',
+  phone: '1234567890',
+},
+{
+  name: 'Lisa S',
+  email: 'lisa@gmail.com',
+  adress: 'calle falsa 123',
+  phone: '1234567890',
+},
+{
+  name: 'Papa Pig',
+  email: 'pepa@gmail.com',
+  adress: 'calle falsa 123',
+  phone: '1234568765'
+}
 
 
 ]
--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,67 @@
+const user = [{
+  name: 'Ada',
+  email: 'ada@gmail.com',
+  adress: 'avenida siempreviva 742',
+  phone: '1234567890',
+},
+{
+  name: 'Lisa S',
+  email: 'lisa@gmail.com',
+  adress: 'calle falsa 123',
+  phone: '1234567890',
+},
+{
+  name: 'Papa Pig',
+  email: 'pepa@gmail.com',
+  adress: 'calle falsa 123',
+  phone: '1234568765'
+}
 
+
+]
+
+const printEmployeeList = (emp) => {
+  console.log(user)
+  let getEmployee = document.getElementById('newEmployee')
+  getEmployee.innerHTML = '';
+  user.forEach((e) => {
+		getEmployee.innerHTML += employee(e);
+	});
+}    
+
+const employee = (e) => `
+    <li class="employeeName" id="name"> <p>${e.name}</p> </li>
+    <li class="employeeEmail" id="email"> <p>${e.email}</p> </li>
+    <li class="employeeAdress" id="adress"> <p>${e.adress}</p> </li>
+    <li class="employeePhone" id="phone"> <p>${e.phone}</p> </li>
+    <li class="employeeActions" id="actions">
+      <a>${e.edit}</a>
+      <a>${e.delet}</a>
+    </li>
+  `
+
+/*   const addNewUser = () => {
+    user.unshift({employeeList()})
+  } */
+
+
+
+
+
+
+
+  
 
 //Modal
 
 const modal = () => {
-    let activeModal = document.getElementById('activeModal')
-    activeModal.classList.remove('modal')
-    activeModal.classList.add('activeModal')
-    }
-    
-    const closeModal = () => {
-      let closeModal = document.getElementById('activeModal')
-      closeModal.classList.remove('activeModal')
-      closeModal.classList.add('modal')
-    }
+  let activeModal = document.getElementById('activeModal')
+  activeModal.classList.remove('modal')
+  activeModal.classList.add('activeModal')
+  }
+  
+  const closeModal = () => {
+    let closeModal = document.getElementById('activeModal')
+    closeModal.classList.remove('activeModal')
+    closeModal.classList.add('modal')
+  }
