3947c0eff99c93ef73525231871d1b4941ed0a81
marcreativo <marcreativo@gmail.com>
2019-11-19 01:16:07 -0300

    review

--- api/user.js
+++ api/user.js
@@ -1,107 +1,105 @@
-const user = [{
-  id: '1',
-  name: 'ada',
-  email: 'ada@gmail.com',
-  address: 'avenida siempreviva 742',
-  phone: '1234567890',
-},
-{
-  id: '2',
-  name: 'lisa s',
-  email: 'lisa@gmail.com',
-  address: 'calle falsa 123',
-  phone: '1234567890',
-},
-{
-  id: '3',
-  name: 'pepa pig',
-  email: 'pepa@gmail.com',
-  address: 'calle falsa 123',
-  phone: '1234568765'
-}
-
-
-]
+const user = [
+	{
+		id: '1',
+		name: 'ada',
+		email: 'ada@gmail.com',
+		address: 'avenida siempreviva 742',
+		phone: '1234567890'
+	},
+	{
+		id: '2',
+		name: 'lisa s',
+		email: 'lisa@gmail.com',
+		address: 'calle falsa 123',
+		phone: '1234567890'
+	},
+	{
+		id: '3',
+		name: 'pepa pig',
+		email: 'pepa@gmail.com',
+		address: 'calle falsa 123',
+		phone: '1234568765'
+	}
+] // identación
 
 const getUser = (req, res, next) => {
-    console.log('pidiendo usuarios')
-    res.status(200).json({user})
-    next()
+	console.log('pidiendo usuarios')
+	res.status(200).json({ user })
+	next()
 }
 
-const getSearchUser= (req, res, next) => {
-  let searchUser = user.filter(e => e.name === req.params.name)
-  
-  if (searchUser) {
-		res.status(200).json(searchUser);
-	}  else {
-		res.status(404).send('no encontramos al usuario');
-	} 
-};
+const getSearchUser = (req, res, next) => {
+	let searchUser = user.filter((e) => e.name === req.params.name)
 
-const deleteId= (req, res, next) => {
-  
-  let index=""
-  let searchUser = user.find((e, i) => {
-    index = i
-    return e.id === req.params.id})
-  
-  
-  if (searchUser) {
-    user.splice(index, 1);
-    res.status(200).json(user)
-    
-    
-	}  else {
-		res.status(404).send('no encontramos al usuario');
-	} 
-};
+	if (searchUser) {
+		res.status(200).json(searchUser)
+	} else {
+		res.status(404).send('no encontramos al usuario')
+	}
+}
 
+const deleteId = (req, res, next) => {
+	let index = ''
+	let searchUser = user.find((e, i) => {
+		index = i
+		return e.id === req.params.id
+	})
 
-const getId= (req, res, next) => {
-  
-  let searchUser = user.filter(e => e.id === req.params.id)
-  
-  if (searchUser) {
-		res.status(200).json(searchUser);
-	}  else {
-		res.status(404).send('no encontramos al usuario');
-	} 
-};
+	if (searchUser) {
+		user.splice(index, 1)
+		res.status(200).json(user)
+	} else {
+		res.status(404).send('no encontramos al usuario')
+	}
+}
+
+const getId = (req, res, next) => {
+	let searchUser = user.filter((e) => e.id === req.params.id)
+
+	if (searchUser) {
+		res.status(200).json(searchUser)
+	} else {
+		res.status(404).send('no encontramos al usuario')
+	}
+}
 
 const postUser = (req, res, next) => {
-	let data = req.body;
-	if (data.hasOwnProperty('name') && data.hasOwnProperty('email') && data.hasOwnProperty('address') && data.hasOwnProperty('phone')) {
-    data.id = Math.random().toString().slice(2,9)
-    user.push(data)
-		res.status('201').json(`recibido con el id ${data.id}`);
+	let data = req.body
+	if (
+		data.hasOwnProperty('name') &&
+		data.hasOwnProperty('email') &&
+		data.hasOwnProperty('address') &&
+		data.hasOwnProperty('phone')
+	) {
+		data.id = Math.random().toString().slice(2, 9)
+		user.push(data)
+		res.status('201').json(`recibido con el id ${data.id}`)
 	} else {
-		res.status('400').json(alert('Todos los campos deben estar completos'));
+		res.status('400').json(alert('Todos los campos deben estar completos'))
 	}
-	next();
-};
+	next()
+}
 
 const patchtUser = (req, res, next) => {
-  let data = req.body;
-  console.log('data', data);
-  
-	let index = '';
+	let data = req.body
+	console.log('data', data)
+
+	let index = ''
 	let resUser = user.find((e, i) => {
-    index = i;
-    return e.id === req.params.id;
-	});
+		index = i
+		return e.id === req.params.id
+	})
 
 	if (resUser) {
-    console.log('resusersdbhfkha', resUser);
-    
-		let editedUser = { ...resUser, ...data };
-		user.splice(index,1);
-    user.push(editedUser);
-    res.status(200).json({data})
+		console.log('resusersdbhfkha', resUser)
+
+		let editedUser = { ...resUser, ...data }
+		user.splice(index, 1)
+		user.push(editedUser)
+		res.status(200).json({ data })
 	} else {
-		res.status(404).send('no encontramos al usuario');
+		res.status(404).send('no encontramos al usuario')
 	}
 }
 
-
-module.exports = {getUser, getSearchUser, postUser, patchtUser, getId, deleteId}
+module.exports = { getUser, getSearchUser, postUser, patchtUser, getId, deleteId }
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,74 +1,61 @@
-let usersData;
-  const initialize = async () => {
-    usersData = await getUser();
-    printEmployeeList(usersData);
-    console.log(usersData)
-  };
+let usersData
+const initialize = async () => {
+	usersData = await getUser()
+	printEmployeeList(usersData)
+	console.log(usersData)
+}
 
 const getUser = () => {
-  return fetch('/api/user')
-    .then((res) => res.json())
-    .then((result) => result.user)
-}  
+	return fetch('/api/user').then((res) => res.json()).then((result) => result.user)
+}
 
-
-
-let lastRequest;
+let lastRequest
 const handleSearch = () => {
-  let name = event.target.value;
-  if ( (event.keyCode === 13 && name !== lastRequest)) {
-    lastRequest = name.toString().toLowerCase();
-    let searchResults = document.getElementById("results")
-    if (container.style.display === "none") {
-      container.style.display = "block";
-    } else {
-      container.style.display = "none";
-      searchResults.style.display ="block"
-    }
-    userSearch(lastRequest)
-    
-    }
-};
-
-
+	let name = event.target.value
+	if (event.keyCode === 13 && name !== lastRequest) {
+		lastRequest = name.toString().toLowerCase()
+		let searchResults = document.getElementById('results')
+		if (container.style.display === 'none') {
+			container.style.display = 'block'
+		} else {
+			container.style.display = 'none'
+			searchResults.style.display = 'block'
+		}
+		userSearch(lastRequest)
+	}
+}
 
 const userSearch = (name) => {
-   fetch(`/api/user/search/${name}`)
-  .then((res) => res.json())
-  .then((r) => {
-    console.log(r);
-    
-    let container = document.getElementById("results")
-    r.forEach(e => {
-        container.innerHTML = employee(e)
-        
-    })
-  })
+	fetch(`/api/user/search/${name}`).then((res) => res.json()).then((r) => {
+		console.log(r)
+
+		let container = document.getElementById('results')
+		r.forEach((e) => {
+			container.innerHTML = employee(e)
+		})
+	})
 }
 
 const printEmployeeList = (emp) => {
-  let getEmployee = document.getElementById('container')
-  getEmployee.innerHTML = '';
-  emp.forEach((e) => {
+	let getEmployee = document.getElementById('container')
+	getEmployee.innerHTML = ''
+	emp.forEach((e) => {
+		getEmployee.innerHTML += employee(e)
+	})
+}
 
-    getEmployee.innerHTML += employee(e);
+const seeAll = () => {
+	let searchResults = document.getElementById('results')
+	if (container.style.display === 'none') {
+		container.style.display = 'block'
+		searchResults.style.display = 'none'
+	} else {
+		container.style.display = 'none'
+	}
+	initialize()
+}
 
-	});
-}    
-
-const seeAll =() =>{
-  let searchResults = document.getElementById("results")
-  if (container.style.display === "none") {
-    container.style.display = "block";
-    searchResults.style.display ="none"
-
-  } else {
-    container.style.display = "none";
-  }
-  initialize()
-} 
-
-
+// si es informacion tabulada debería verse en tablas
 const employee = (e) => `
     <ul class="employeeList" id="newEmployee"> 
     <li class="employeeName" id="name"  > <p>${e.name}</p> </li>
@@ -83,157 +70,145 @@ const employee = (e) => `
     </ul>
   `
 
-  //eliminar
+//eliminar
 
 var removeElement = (id) => {
-
-  alert('¿Estás segurx de borrar este usuario?')
-  let inputEmployeeName= document.getElementById('name')
-    let inputEmployeeEmail= document.getElementById('email')
-    let inputEmployeeAddress= document.getElementById('address')
-    let inputEmployeePhone= document.getElementById('phone')
-
-  const payload = {
-    name: inputEmployeeName.value,
-    email: inputEmployeeEmail.value,
-    address: inputEmployeeAddress.value,
-    phone: inputEmployeePhone.value,
-  }
-fetch(`/api/user/delete/${id}`, {
-  method: 'DELETE',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(payload)
-})
-  .then((res) => res.json())
-  .then((result) => {
-
-   initialize()})
+	alert('¿Estás segurx de borrar este usuario?')
+	let inputEmployeeName = document.getElementById('name')
+	let inputEmployeeEmail = document.getElementById('email')
+	let inputEmployeeAddress = document.getElementById('address')
+	let inputEmployeePhone = document.getElementById('phone')
+	// para qué usan el payload en el delete?
+	const payload = {
+		name: inputEmployeeName.value,
+		email: inputEmployeeEmail.value,
+		address: inputEmployeeAddress.value,
+		phone: inputEmployeePhone.value
+	}
+	fetch(`/api/user/delete/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(payload)
+	})
+		.then((res) => res.json())
+		.then((result) => {
+			initialize()
+		})
 }
-    
+
 // editar
 const editElement = (id) => {
-  let inputEmployeeName= document.getElementById('name')
-    let inputEmployeeEmail= document.getElementById('email')
-    let inputEmployeeAddress= document.getElementById('address')
-    let inputEmployeePhone= document.getElementById('phone')
-    let newEmployeeName = inputEmployeeName.value
-    let newEmployeeEmail = inputEmployeeEmail.value
-    let newEmployeeAddress = inputEmployeeAddress.value
-    let newEmployeePhone = inputEmployeePhone.value
-    
-    const payload = {
-      name: newEmployeeName,
-      email: newEmployeeEmail,
-      address: newEmployeeAddress,
-     phone: newEmployeePhone
-    }
+	// la edición cambia todos los valores del usuario a 0 por alguna razón.
+	let inputEmployeeName = document.getElementById('name')
+	let inputEmployeeEmail = document.getElementById('email')
+	let inputEmployeeAddress = document.getElementById('address')
+	let inputEmployeePhone = document.getElementById('phone')
+	let newEmployeeName = inputEmployeeName.value
+	let newEmployeeEmail = inputEmployeeEmail.value
+	let newEmployeeAddress = inputEmployeeAddress.value
+	let newEmployeePhone = inputEmployeePhone.value
+
+	const payload = {
+		name: newEmployeeName,
+		email: newEmployeeEmail,
+		address: newEmployeeAddress,
+		phone: newEmployeePhone
+	}
 	fetch(`api/user/${id}`, {
 		method: 'PATCH',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(payload)
-  })
-  .then((res) => res.json())
-  .then((result) => {
-    modal(payload)
+	})
+		.then((res) => res.json())
+		.then((result) => {
+			modal(payload)
 
-   initialize()})
-
-  
-
-};
-
-
-
+			initialize()
+		})
+}
 
 //Modal
 
 const modal = () => {
-  let activeModal = document.getElementById('activeModal')
-  activeModal.classList.remove('modal')
-  activeModal.classList.add('activeModal')
-  }
-  
-  const closeModal = () => {
-    let closeModal = document.getElementById('activeModal')
-    closeModal.classList.remove('activeModal')
-    closeModal.classList.add('modal')
-  }
+	let activeModal = document.getElementById('activeModal')
+	activeModal.classList.remove('modal')
+	activeModal.classList.add('activeModal')
+}
+
+const closeModal = () => {
+	let closeModal = document.getElementById('activeModal')
+	closeModal.classList.remove('activeModal')
+	closeModal.classList.add('modal')
+}
 
 // validacion
+// las validaciones no estarían ocurriendo
+const validation = (e) => {
+	if (e.length > 3) {
+		return true
+	} else {
+		return false
+	}
+}
 
-  const validation = (e) => {
-    if (e.length >3) {
-      return true
-  }else {
-    return false
-  }
-  }
-  
-  
-   let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  const emailValid = (e) => {
-    validEmail.test(e)
-  } 
-
-  
+let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailValid = (e) => {
+	validEmail.test(e)
+}
 
 const numberValid = (num) => {
-  let validNumber = /^([0-9])*$/
-     return validNumber.test(num)
-  }
-  
+	let validNumber = /^([0-9])*$/
+	return validNumber.test(num)
+}
 
-// nuevo empleado 
+// nuevo empleado
 
-  const addNewEmployee = () => {
-    event.preventDefault()
-    let inputEmployeeName= document.getElementById('nameModal')
-    let inputEmployeeEmail= document.getElementById('emailModal')
-    let inputEmployeeAddress= document.getElementById('addressModal')
-    let inputEmployeePhone= document.getElementById('phoneModal')
+const addNewEmployee = () => {
+	event.preventDefault()
+	//la captura de valores como estos está repetida en varios lugares, se podria extraer en una función
+	let inputEmployeeName = document.getElementById('nameModal')
+	let inputEmployeeEmail = document.getElementById('emailModal')
+	let inputEmployeeAddress = document.getElementById('addressModal')
+	let inputEmployeePhone = document.getElementById('phoneModal')
 
-    let newEmployeeName = inputEmployeeName.value
-    let newEmployeeEmail = inputEmployeeEmail.value
-    let newEmployeeAddress = inputEmployeeAddress.value
-    let newEmployeePhone = inputEmployeePhone.value
+	let newEmployeeName = inputEmployeeName.value
+	let newEmployeeEmail = inputEmployeeEmail.value
+	let newEmployeeAddress = inputEmployeeAddress.value
+	let newEmployeePhone = inputEmployeePhone.value
 
-   /*  if (validation(newEmployeeName) && validation(newEmployeeAddress) && numberValid(newEmployeePhone) && validateEmail(newEmployeeEmail)) 
+	// guarda con el codigo comentado
+	/*  if (validation(newEmployeeName) && validation(newEmployeeAddress) && numberValid(newEmployeePhone) && validateEmail(newEmployeeEmail)) 
       {
  */
 
-      const payload = {
-        name: newEmployeeName.toString().toLowerCase(),
-        email: newEmployeeEmail,
-        address: newEmployeeAddress,
-        phone: newEmployeePhone
-      }
-    fetch(`api/user`, {
-      method:'POST',
-      headers: {
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify(payload)
-    }) 
-    .then((res) => res.json())
-    .then((result) =>{
-      inputEmployeeName.value= ''
-      inputEmployeeEmail.value= ''
-      inputEmployeeAddress.value= ''
-      inputEmployeePhone.value= ''
-      
-      closeModal()
-      initialize()
-     
-    })
-    /* }  else {
+	const payload = {
+		name: newEmployeeName.toString().toLowerCase(),
+		email: newEmployeeEmail,
+		address: newEmployeeAddress,
+		phone: newEmployeePhone
+	}
+	fetch(`api/user`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(payload)
+	})
+		.then((res) => res.json())
+		.then((result) => {
+			inputEmployeeName.value = ''
+			inputEmployeeEmail.value = ''
+			inputEmployeeAddress.value = ''
+			inputEmployeePhone.value = ''
+
+			closeModal()
+			initialize()
+		})
+	/* }  else {
     alert('Campos incompletos')
   }  */
-     
-  }
-
-
-  
+}
